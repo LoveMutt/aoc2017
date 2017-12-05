c277c01546c2f8a674a37f640f978954cb492d81
mittens <jed.mitten@fireeye.com>
2017-12-04 17:47:39 -0800

    day 2 part 2 solution and tests

--- day02.py
+++ day02.py
@@ -17,6 +17,27 @@ def checksum(rows):
 	return sum
 
 
+def checksum_part2(rows):
+	sum = 0
+	for row in rows:
+		found = False
+		srow = sorted(row)
+		log.debug('sorted row: {}'.format(srow))
+		for i in range(len(srow)):
+			r_list = srow[i+1:]
+			for j in r_list:
+				log.debug('testing {} against {}'.format(srow[i], j))
+				if evenly_divisible(srow[i], j):
+					sum += j / srow[i]
+					found = True
+					break  # break to iterate over next row
+			if found:
+				break  # break to iterate over next row
+	return sum
+
+
+
+
 def parse_input(intext):
 	inlist = []
 	for textrow in intext.split('\n'):
@@ -26,11 +47,16 @@ def parse_input(intext):
 	return inlist
 
 
+def evenly_divisible(a, b):
+	return not b % a
+
+
 def main():
 	inlist = parse_input(common.read_input(02))
-	print('The answer is: {}'.format(checksum(inlist)))
+	print('The answer to part 1 is: {}'.format(checksum(inlist)))
+	print('The answer to part 2 is: {}'.format(checksum_part2(inlist)))
 
 
 if __name__ == '__main__':
-	log = common.get_logger('day02')
+	log = common.get_logger('day02', level='INFO')
 	main()
--- test_day02.py
+++ test_day02.py
@@ -11,6 +11,11 @@ class TestDay02(unittest.TestCase):
 7	5	3
 2	4	6	8'''
 
+	test2 = \
+'''5	9	2	8
+9	4	7	3
+3	8	6	5'''
+
 	def test_parse_input(self):
 		A = parse_input(self.test)
 		self.assertIsInstance(A, list)
@@ -22,3 +27,12 @@ class TestDay02(unittest.TestCase):
 	def test_checksum(self):
 		cs = checksum(parse_input(self.test))
 		self.assertEqual(cs, 18)
+
+	def test_checksum_part2(self):
+		cs = checksum_part2(parse_input(self.test2))
+		self.assertEqual(cs, 9)
+
+	def test_evenly_divisible(self):
+		self.assertTrue(evenly_divisible(2, 6))
+		self.assertTrue(evenly_divisible(3, 6))
+		self.assertFalse(evenly_divisible(3, 4))
