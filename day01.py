d34a91e32ae3045888bea6b34d18231e9e6929ff
mittens <jed.mitten@fireeye.com>
2017-12-04 17:55:59 -0800

    day 1 part 2 solution and tests

--- day01.py
+++ day01.py
@@ -3,23 +3,35 @@ import common
 log = common.get_logger(__name__)
 
 
-def matches_next(L, i):
-	j = (i + 1) % len(L)
+def matches_next(L, i, step=1):
+	j = (i + step) % len(L)
 	return L[i] == L[j]
 
 
 def sum_matches(L):
-	sum = 0
+	match_sum = 0
 	for i in range(len(L)):
 		if matches_next(L, i):
-			sum += L[i]
-	return sum
+			match_sum += L[i]
+	return match_sum
+
+
+def sum_matches_part2(L):
+	match_sum = 0
+	for i in range(len(L)):
+		step = len(L) / 2
+		if matches_next(L, i, step=step):
+			match_sum += L[i]
+	return match_sum
+
 
 def main():
 	input = common.read_input(01)
 	input_array = [int(c) for c in input]
 	answer = sum_matches(input_array)
-	print('The answer is: {}'.format(answer))
+	answer2 = sum_matches_part2(input_array)
+	print('The part 1 answer is: {}'.format(answer))
+	print('The part 2 answer is: {}'.format(answer2))
 
 
 if __name__ == '__main__':
--- test_day01.py
+++ test_day01.py
@@ -1,15 +1,20 @@
 import unittest
 
-from day01 import matches_next, sum_matches
+from day01 import *
 from common import get_logger
 
 log = get_logger(__name__)
 
-class TestDay01(unittest.TestCase):
+class TestDay(unittest.TestCase):
 	test1 = [1, 1, 2, 2]
 	test2 = [1, 1, 1, 1]
 	test3 = [1, 2, 3, 4]
 	test4 = [9, 1, 2, 1, 2, 1, 2, 9]
+	test5 = [1, 2, 1, 2]
+	test6 = [1, 2, 2, 1]
+	test7 = [1, 2, 3, 4, 2, 5]
+	test8 = [1, 2, 3, 1, 2, 3]
+	test9 = [1, 2, 1, 3, 1, 4, 1, 5]
 
 	def test_matches_next(self):
 		self.assertTrue(matches_next(self.test1, 0))
@@ -21,3 +26,10 @@ class TestDay01(unittest.TestCase):
 		self.assertEqual(sum_matches(self.test2), 4)
 		self.assertEqual(sum_matches(self.test3), 0)
 		self.assertEqual(sum_matches(self.test4), 9)
+
+	def test_sum_matches_part2(self):
+		self.assertEqual(sum_matches_part2(self.test5), 6)
+		self.assertEqual(sum_matches_part2(self.test6), 0)
+		self.assertEqual(sum_matches_part2(self.test7), 4)
+		self.assertEqual(sum_matches_part2(self.test8), 12)
+		self.assertEqual(sum_matches_part2(self.test9), 4)
