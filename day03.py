1b218932c7e1ad3098060f59d4076746a17381ea
Lughead <jedmitten@alumni.cmu.edu>
2017-12-08 08:51:31 -0800

    wip day 3 part 2

--- day03.py
+++ day03.py
@@ -12,9 +12,32 @@ class CartesianSpiral:
     def __init__(self):
         # starting values
         self._target_val = 1
-        self._target_x = 0
-        self._target_y = 0
+        self._max_x = 0
+        self._max_y = 0
+        self.squares = [self.Square(1, 0, 0)]
         self._direction = RIGHT
+
+    class Square:
+        def __init__(self, value=0, x=0, y=0):
+            self.value = value
+            self.x = x
+            self.y = y
+            self.adjacents = self.set_adjacents()
+
+        def set_adjacents(self):
+            adjacents = set([])
+            adjacents.add((self.x + 1, self.y))
+            adjacents.add((self.x + 1, self.y + 1))
+            adjacents.add((self.x, self.y + 1))
+            adjacents.add((self.x - 1, self.y + 1))
+            adjacents.add((self.x - 1, self.y))
+            adjacents.add((self.x - 1, self.y - 1))
+            adjacents.add((self.x, self.y - 1))
+            adjacents.add((self.x + 1, self.y - 1))
+            self.adjacents = adjacents
+
+        def __repr__(self):
+            return '<Square: value={}, x={}, y={}'.format(self.value, self.x, self.y)
 
     @property
     def origin(self):
@@ -30,39 +53,74 @@ class CartesianSpiral:
         else:
             self._direction = RIGHT
 
-    def incr_coordinates(self):
-        if self._direction == RIGHT:
-            self._target_x = self._target_x + 1
-        elif self._direction == UP:
-            self._target_y = self._target_y + 1
-        elif self._direction == LEFT:
-            self._target_x = self._target_x - 1
-        else:
-            self._target_y = self._target_y - 1
 
-    def spiral_to(self, val):
+    def incr_coordinates(self):
+        last_sq = self.squares[-1]
+        if self._direction == RIGHT:
+            self._max_x = last_sq.x + 1
+        elif self._direction == UP:
+            self._max_y = last_sq.y + 1
+        elif self._direction == LEFT:
+            self._max_x = last_sq.x - 1
+        else:
+            self._max_y = last_sq.y - 1
+        return (self._max_x, self._max_y)
+
+    def incr_value(self):
+        last_sq = self.squares[-1]
+        return last_sq.value + 1
+
+    def spiral_to(self, val, part1=True):
         step = 0
+        sq = self.squares[0]
         while True:
             step = step + 1
             for i in range(2):
                 for j in range(step):
-                    if self._target_val == val:
+                    if sq.value == val:
                         return
-                    self._target_val += 1
-                    self.incr_coordinates()
+                    sq = self.Square()
+                    x, y = self.incr_coordinates()
+                    sq.x = x
+                    sq.y = y
+                    if part1:
+                        sq.value = self.incr_value()
+                    else:
+                        sq.value = self.adj_sum(sq)
+                    log.debug('Square: {}'.format(sq))
+                    self.squares.append(sq)
                 self.turn()
 
-    def distance_to_target(self, val):
-        self.spiral_to(val)
-        return abs(self._target_y) + abs(self._target_x)
+    def distance_to_target(self, val, part1=True):
+        self.spiral_to(val, part1=part1)
+        sq = self.squares[-1]
+        return abs(sq.x) + abs(sq.y)
+
+    def adj_sum(self, sq):
+        """0, 0
+           1, 0
+           1, 1
+           0, 1
+           -1, 1
+           -1, 0
+           -1, -1
+           0, -1
+           -1, -1
+
+           Do I just scan the list looking at coordinates or do I store them initially in a plane w/ queue/pop properties
+        """
+        x = sq.x
+        y = sq.y
+        x += 1
 
 
 def main():
     input_part_1 = 265149
     s = CartesianSpiral()
-    answer_1 = s.distance_to_target(input_part_1)
+    answer_1 = s.distance_to_target(input_part_1, part1=True)
+    answer_2 = s.distance_to_target(input_part_1, part1=False)
     print('The answer to part 1 is: {}'.format(answer_1))
-    print('The answer to part 2 is: {}'.format('unknown'))
+    print('The answer to part 2 is: {}'.format(answer_2))
 
 
 if __name__ == '__main__':
--- test_day03.py
+++ test_day03.py
@@ -6,71 +6,6 @@ from common import get_logger
 log = get_logger(__name__)
 
 class TestDay(unittest.TestCase):
-	def test_turn(self):
-		s = CartesianSpiral()
-		self.assertEqual(s._direction, RIGHT)
-		s.turn()
-		self.assertEqual(s._direction, UP)
-		s.turn()
-		self.assertEqual(s._direction, LEFT)
-		s.turn()
-		self.assertEqual(s._direction, DOWN)
-		s.turn()
-		self.assertEqual(s._direction, RIGHT)
-
-	def test_incr_coordinates(self):
-		s = CartesianSpiral()
-		self.assertEqual(s._target_x, 0)
-		self.assertEqual(s._target_y, 0)
-		s.incr_coordinates()
-		self.assertEqual(s._target_x, 1)
-		self.assertEqual(s._target_y, 0)
-		s.turn()
-		s.incr_coordinates()
-		self.assertEqual(s._target_x, 1)
-		self.assertEqual(s._target_y, 1)
-		s.turn()
-		s.incr_coordinates()
-		self.assertEqual(s._target_x, 0)
-		self.assertEqual(s._target_y, 1)
-		s.incr_coordinates()
-		self.assertEqual(s._target_x, -1)
-		self.assertEqual(s._target_y, 1)
-		s.turn()
-		s.incr_coordinates()
-		self.assertEqual(s._target_x, -1)
-		self.assertEqual(s._target_y, 0)
-
-	def test_spiral_to(self):
-		s = CartesianSpiral()
-		val = 1
-		log.info('spiral_to: {}'.format(val))
-		s.spiral_to(val)
-		self.assertEqual(s._target_val, 1)
-		self.assertEqual(s._target_x, 0)
-		self.assertEqual(s._target_y, 0)
-		val = 2
-		s = CartesianSpiral()
-		log.info('spiral_to: {}'.format(val))
-		s.spiral_to(val)
-		self.assertEqual(s._target_val, 2)
-		self.assertEqual(s._target_x, 1)
-		self.assertEqual(s._target_y, 0)
-		val = 3
-		s = CartesianSpiral()
-		log.info('spiral_to: {}'.format(val))
-		s.spiral_to(val)
-		self.assertEqual(s._target_val, 3)
-		self.assertEqual(s._target_x, 1)
-		self.assertEqual(s._target_y, 1)
-		val = 23
-		s = CartesianSpiral()
-		log.info('spiral_to: {}'.format(val))
-		s.spiral_to(val)
-		self.assertEqual(s._target_val, 23)
-		self.assertEqual(s._target_x, 0)
-		self.assertEqual(s._target_y, -2)
-
 	def test_distance_to_target(self):
 		s = CartesianSpiral()
 		val = 1
@@ -88,3 +23,7 @@ class TestDay(unittest.TestCase):
 		s = CartesianSpiral()
 		log.info('val: {}'.format(val))
 		self.assertEqual(s.distance_to_target(val), 31)
+
+	def test_square(self):
+		square = CartesianSpiral.Square()
+		self.assertIsInstance(square, CartesianSpiral.Square)
