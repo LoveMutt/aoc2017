ffb2ca83ecfadb1798e214c183cb2a234ff382bd
mittens <jed.mitten@fireeye.com>
2017-12-08 22:29:36 -0800

    soln and tests for day03 ** THIS IS NOT AN EFFICIENT SOLN **

--- day03.py
+++ day03.py
@@ -1,4 +1,6 @@
 import common
+import sys
+from collections import namedtuple
 
 log = common.get_logger(__name__)
 
@@ -8,21 +10,25 @@ UP = 'up'
 DOWN = 'down'
 
 
+CoordSquare = namedtuple('CoordSquare', 'coordinates square')
+
+
 class CartesianSpiral:
     def __init__(self):
         # starting values
-        self._target_val = 1
+        self._target_val = 0
         self._max_x = 0
         self._max_y = 0
-        self.squares = [self.Square(1, 0, 0)]
-        self._direction = RIGHT
+        self.coodsquares = []
+        self._direction = ''
+        self.reset()
 
     class Square:
         def __init__(self, value=0, x=0, y=0):
             self.value = value
             self.x = x
             self.y = y
-            self.adjacents = self.set_adjacents()
+            self.set_adjacents()
 
         def set_adjacents(self):
             adjacents = set([])
@@ -43,6 +49,20 @@ class CartesianSpiral:
     def origin(self):
         return (0, 0)
 
+    @property
+    def origin_square(self):
+        x, y = self.origin
+        return self.Square(1, x, y)
+
+    def reset(self):
+        self._target_val = 1
+        self.max_val = -1
+        self._max_x = 0
+        self._max_y = 0
+        csq = CoordSquare(coordinates=self.origin, square=self.origin_square)
+        self.coodsquares = [csq]
+        self._direction = RIGHT
+
     def turn(self):
         if self._direction == RIGHT:
             self._direction = UP
@@ -55,71 +75,107 @@ class CartesianSpiral:
 
 
     def incr_coordinates(self):
-        last_sq = self.squares[-1]
+        last_sq = self.coodsquares[-1]  # type: CoordSquare
         if self._direction == RIGHT:
-            self._max_x = last_sq.x + 1
+            self._max_x = last_sq.square.x + 1
         elif self._direction == UP:
-            self._max_y = last_sq.y + 1
+            self._max_y = last_sq.square.y + 1
         elif self._direction == LEFT:
-            self._max_x = last_sq.x - 1
+            self._max_x = last_sq.square.x - 1
         else:
-            self._max_y = last_sq.y - 1
+            self._max_y = last_sq.square.y - 1
         return (self._max_x, self._max_y)
 
-    def incr_value(self):
-        last_sq = self.squares[-1]
-        return last_sq.value + 1
+    def part1_val(self):
+        last_sq = self.coodsquares[-1]  # type: CoordSquare
+        return last_sq.square.value + 1
 
-    def spiral_to(self, val, part1=True):
+    def spiral_to(self, val=0, index=0, part1=True):
+        # type: (int) -> None
+        """
+        Spiral until either a value is reached or an index is reached. Part1 tells this function how to assign a value
+        """
+        self.reset()
+        if index:
+            part1 = False  # shortcut to not rewrite code
         step = 0
-        sq = self.squares[0]
+        csq = self.coodsquares[0]  # type: CoordSquare
         while True:
+            log.debug('increasing step to move around existing coodsquares')
             step = step + 1
             for i in range(2):
                 for j in range(step):
-                    if sq.value == val:
-                        return
-                    sq = self.Square()
+                    if val:
+                        if csq.square.value == val:
+                            return
+                    elif index:
+                        if len(self.coodsquares) == index:
+                            return
                     x, y = self.incr_coordinates()
-                    sq.x = x
-                    sq.y = y
                     if part1:
-                        sq.value = self.incr_value()
+                        nxt_val = self.part1_val()
                     else:
-                        sq.value = self.adj_sum(sq)
-                    log.debug('Square: {}'.format(sq))
-                    self.squares.append(sq)
+                        new_sq = CartesianSpiral.Square(value=0, x=x, y=y)
+                        nxt_val = self.part2_val(new_sq)
+                    self.max_val = nxt_val
+                    csq = CoordSquare(coordinates=(x, y), square=self.Square(value=nxt_val, x=x, y=y))
+                    log.debug('Square: {}'.format(csq))
+                    self.coodsquares.append(csq)
                 self.turn()
 
     def distance_to_target(self, val, part1=True):
         self.spiral_to(val, part1=part1)
-        sq = self.squares[-1]
-        return abs(sq.x) + abs(sq.y)
+        csq = self.coodsquares[-1]  # type: CoordSquare
+        x, y = csq.coordinates
+        return abs(x) + abs(y)
 
-    def adj_sum(self, sq):
-        """0, 0
-           1, 0
-           1, 1
-           0, 1
-           -1, 1
-           -1, 0
-           -1, -1
-           0, -1
-           -1, -1
-
-           Do I just scan the list looking at coordinates or do I store them initially in a plane w/ queue/pop properties
+    def part2_val(self, sq):
+        # type: (CartesianSpiral.Square) -> int
         """
-        x = sq.x
-        y = sq.y
-        x += 1
+        Sum the adjacent coodsquares to a square
+        """
+        sumval = 0
+        for adj_coord in sq.adjacents:
+            adjsq = self.get_square_by_coord(adj_coord)
+            adj_val = adjsq.value
+            sumval += adj_val
+        return sumval
+
+    def get_square_by_coord(self, coord_tup):
+        # type: (tuple) -> CartesianSpiral.Square
+        for squareplot in self.coodsquares:  # type: CoordSquare
+            if squareplot.coordinates == coord_tup:
+                return squareplot.square
+        return CartesianSpiral.Square(0, -1000000000, -100000000)
+
+    def get_coordsquare(self, num):
+        # type: (int) -> CoordSquare
+        """
+        Allow indexing by 1-based
+        """
+        return self.coodsquares[num - 1]
 
 
 def main():
-    input_part_1 = 265149
+    input_val = 265149
     s = CartesianSpiral()
-    answer_1 = s.distance_to_target(input_part_1, part1=True)
-    answer_2 = s.distance_to_target(input_part_1, part1=False)
-    print('The answer to part 1 is: {}'.format(answer_1))
+    # answer_1 = s.distance_to_target(input_val, part1=True)
+    # print('The answer to part 1 is: {}'.format(answer_1))
+    n = 0
+    while True:
+        n += 100
+        s.spiral_to(index=n, part1=False)
+        if not s.max_val > input_val:
+            continue
+        else:
+            answer_2 = -1
+            values = [t.square.value for t in s.coodsquares]
+            values = sorted(values)
+            for v in values:
+                if v > input_val:
+                    answer_2 = v
+                    break
+            break
     print('The answer to part 2 is: {}'.format(answer_2))
 
 
--- test_day03.py
+++ test_day03.py
@@ -5,25 +5,55 @@ from common import get_logger
 
 log = get_logger(__name__)
 
-class TestDay(unittest.TestCase):
-	def test_distance_to_target(self):
-		s = CartesianSpiral()
-		val = 1
-		log.info('val: {}'.format(val))
-		self.assertEqual(s.distance_to_target(val), 0)
-		val = 12
-		s = CartesianSpiral()
-		log.info('val: {}'.format(val))
-		self.assertEqual(s.distance_to_target(val), 3)
-		val = 23
-		s = CartesianSpiral()
-		log.info('val: {}'.format(val))
-		self.assertEqual(s.distance_to_target(val), 2)
-		val = 1024
-		s = CartesianSpiral()
-		log.info('val: {}'.format(val))
-		self.assertEqual(s.distance_to_target(val), 31)
 
-	def test_square(self):
-		square = CartesianSpiral.Square()
-		self.assertIsInstance(square, CartesianSpiral.Square)
+class TestDay(unittest.TestCase):
+    def test_distance_to_target(self):
+        s = CartesianSpiral()
+        val = 1
+        log.info('val: {}'.format(val))
+        self.assertEqual(s.distance_to_target(val), 0)
+        val = 12
+        s = CartesianSpiral()
+        log.info('val: {}'.format(val))
+        self.assertEqual(s.distance_to_target(val), 3)
+        val = 23
+        s = CartesianSpiral()
+        log.info('val: {}'.format(val))
+        self.assertEqual(s.distance_to_target(val), 2)
+        val = 1024
+        s = CartesianSpiral()
+        log.info('val: {}'.format(val))
+        self.assertEqual(s.distance_to_target(val), 31)
+
+    def test_square(self):
+        square = CartesianSpiral.Square()
+        self.assertIsInstance(square, CartesianSpiral.Square)
+
+    def test_square_adjacents(self):
+        sq = CartesianSpiral.Square(1, 0, 0)
+        adj_set = {(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)}
+        self.assertTrue(set(sq.adjacents).issubset(adj_set))
+        self.assertTrue(set(sq.adjacents).issuperset(adj_set))
+
+    def test_square_adj(self):
+        cs = CartesianSpiral()
+        index = 1
+        cs.spiral_to(index=index, part1=False)
+        csq = cs.get_coordsquare(index)  # type: CoordSquare
+        self.assertEqual(csq.square.value, 1)
+        index = 2
+        cs.spiral_to(index=index, part1=False)
+        csq = cs.get_coordsquare(index)  # type: CoordSquare
+        self.assertEqual(csq.square.value, 1)
+        index = 3
+        cs.spiral_to(index=index, part1=False)
+        csq = cs.get_coordsquare(index)  # type: CoordSquare
+        self.assertEqual(csq.square.value, 2)
+        index = 4
+        cs.spiral_to(index=index, part1=False)
+        csq = cs.get_coordsquare(index)  # type: CoordSquare
+        self.assertEqual(csq.square.value, 4)
+        index = 5
+        cs.spiral_to(index=index, part1=False)
+        csq = cs.get_coordsquare(index)  # type: CoordSquare
+        self.assertEqual(csq.square.value, 5)
