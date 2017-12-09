f6b15544bfedccf6524d813bcda0e71d6688d5d6
mittens <jed.mitten@fireeye.com>
2017-12-09 03:00:05 -0500

    fix common functions

--- common.py
+++ common.py
@@ -2,12 +2,14 @@ import logging
 
 
 def get_logger(name, level='DEBUG'):
-	loglevel = logging.getLevelName(level)
-	logging.basicConfig(level=loglevel)
-	return logging.getLogger(name)
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+    loglevel = logging.getLevelName(level)
+    logging.basicConfig(level=loglevel)
+    return logging.getLogger(name)
 
 
 def read_input(day):
-	fn = 'input_day{:02}.txt'.format(day)
-	with open(fn) as f:
-		return f.read()
+    fn = 'input_day{:02}.txt'.format(day)
+    with open(fn) as f:
+        return f.read()
