3324611f32ef4e147cc27e742da84b90bc4cfe43
mittens <jed.mitten@fireeye.com>
2017-12-09 03:50:47 -0500

    fix test input and expected answer

--- test_day05.py
+++ test_day05.py
@@ -5,10 +5,10 @@ from common import get_logger
 
 log = get_logger(__name__)
 
-TEST_INPUT = ['0', '3', '0', '1', '-3']
+TEST_INPUT = [0, 3, 0, 1, -3]
 
 class TestDay(unittest.TestCase):
     def test_proc(self):
         js = JmpSet(jmps=TEST_INPUT)
         js.proc()
-        self.assertEqual(js.num_processed, 4)
+        self.assertEqual(js.steps, 5)
