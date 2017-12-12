f47d64801b2cb0888d1ead69e85c45eec37d952f
mittens <jed.mitten@fireeye.com>
2017-12-11 23:06:14 -0800

    new functionality and tests

--- day08.py
+++ day08.py
@@ -1,22 +1,44 @@
 import common
+import sys
 
 log = common.get_logger(__name__)
 
 
 class Register:
-    def __init__(self, id='', value=0):
+    def __init__(self, id=''):
         self.id = id
-        self.value = value
+        self.value = 0
+
+    def exec_inst(self, s_inst):
+        parts = s_inst.split(' ')
+        if len(parts) == 3:
+            _, direction, value = parts
+        else:
+            direction, value = parts
+        value = int(value)
+        if direction == 'inc':
+            self.value += value
+        elif direction == 'dec':
+            self.value -= value
+        else:
+            log.error('Bad instruction: {}'.format(s_inst))
+            sys.exit(3)
 
 
 class RegisterCollection:
     def __init__(self, registers=None):
-        self.registers = registers
-        if self.registers is None:
-            self.registers = []
+        self.registers = []
+        if isinstance(registers, list):
+            for r in registers:
+                self.add_register(r)
+
+    @property
+    def ids(self):
+        return [r.id for r in self.registers]
 
     def add_register(self, register):
-        self.registers.append(register)
+        if not register.id in [r.id for r in self.registers]:
+            self.registers.append(register)
 
     @staticmethod
     def parse_instruction(s_inst):
@@ -41,13 +63,29 @@ class RegisterCollection:
         b = eval(s_test)
         return b
 
-    def do_instruction(self, s):
-        tgt, mod, val, c_reg, c_mod, c_val = parse_input(s)
-
+    def exec_instruction(self, s):
+        # type: (str) -> None
+        tgt_reg, direction, val, s_cond = RegisterCollection.parse_instruction(s)
+        if self.test_condition(s_cond):
+            r = self.get_register_by_id(tgt_reg)  # type: Register
+            s_inst = '{} {}'.format(direction, val)
+            r.exec_inst(s_inst)
 
 
 def parse_input(intext):
     return intext.split('\n')
+
+
+def init_collection_from_input(intext):
+    lines = parse_input(intext)
+    rc = RegisterCollection()
+    for line in lines:
+        r_name1, _, _, _, r_name2, _, _ = line.split(' ')
+        reg1 = Register(id=r_name1)
+        reg2 = Register(id=r_name2)
+        rc.add_register(register=reg1)
+        rc.add_register(register=reg2)
+    return rc
 
 
 def main():
--- test_day08.py
+++ test_day08.py
@@ -6,14 +6,48 @@ from common import get_logger
 log = get_logger(__name__)
 
 
+def get_test_input():
+    return """b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10"""
+
+
 class TestDay(unittest.TestCase):
     def setUp(self):
-        self.ra = Register(id='a', value=5)
-        self.rb = Register(id='b', value=3)
+        self.ra = Register(id='a')
+        self.rb = Register(id='b')
         self.registers = RegisterCollection(registers=[self.ra, self.rb])
 
     def test_test_conditon(self):
+        self.ra.value = 5
         cond = 'a == 5'
         self.assertTrue(self.registers.test_condition(cond))
         cond = 'a == 4'
         self.assertFalse(self.registers.test_condition(cond))
+
+    def test_reg_inst(self):
+        self.ra.exec_inst('inc 5')
+        self.assertEqual(5, self.ra.value)
+        self.ra.exec_inst('dec 4')
+        self.assertEqual(1, self.ra.value)
+
+    def test_get_register_by_id(self):
+        id = 'a'
+        self.assertEqual(self.ra, self.registers.get_register_by_id(id))
+        id = 'b'
+        self.assertEqual(self.rb, self.registers.get_register_by_id(id))
+
+    def test_exec_instruction(self):
+        self.assertEqual(0, self.registers.get_register_by_id('a'))
+        inst = 'a inc 1 if a == 0'
+        self.assertEqual(0, self.registers.get_register_by_id('a'))
+        self.registers.exec_instruction(inst)
+        self.assertEqual(1, self.registers.get_register_by_id('a'))
+
+    def test_init_collection(self):
+        rc = init_collection_from_input(get_test_input())
+        self.assertEqual(3, len(rc.registers))
+        self.assertTrue('a' in rc.ids)
+        self.assertTrue('b' in rc.ids)
+        self.assertTrue('c' in rc.ids)
