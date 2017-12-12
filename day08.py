3ce3401fb3e75802ae6be27b6865847844953c7f
mittens <jed.mitten@fireeye.com>
2017-12-11 22:45:18 -0800

    beginning classes for Register and RegisterCollection

--- day08.py
+++ day08.py
@@ -3,6 +3,49 @@ import common
 log = common.get_logger(__name__)
 
 
+class Register:
+    def __init__(self, id='', value=0):
+        self.id = id
+        self.value = value
+
+
+class RegisterCollection:
+    def __init__(self, registers=None):
+        self.registers = registers
+        if self.registers is None:
+            self.registers = []
+
+    def add_register(self, register):
+        self.registers.append(register)
+
+    @staticmethod
+    def parse_instruction(s_inst):
+        # type: (str) -> list(str)
+        predicate, conditon = [s.strip() for s in s_inst.split('if')]
+        target_reg, direction, value = predicate.split(' ')
+        return target_reg, direction, value, conditon
+
+    def get_register_by_id(self, id):
+        registers = [r for r in self.registers if r.id == id]
+        return registers[0]
+
+    def test_condition(self, s_cond):
+        reg_id, cond, value = s_cond.split(' ')
+        register = self.get_register_by_id(reg_id)
+        if not isinstance(register.value, int) or \
+                not isinstance(int(value), int) or \
+                ';' in s_cond or 'import' in s_cond:
+            log.error('Stop hacking me')
+            sys.exit(2)
+        s_test = '{} {} {}'.format(register.value, cond, value)
+        b = eval(s_test)
+        return b
+
+    def do_instruction(self, s):
+        tgt, mod, val, c_reg, c_mod, c_val = parse_input(s)
+
+
+
 def parse_input(intext):
     return intext.split('\n')
 
