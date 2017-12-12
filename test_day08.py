97daef4320f9c8b5a325b85ab42cab307c2fb8e6
mittens <jed.mitten@fireeye.com>
2017-12-11 22:45:33 -0800

    testing for test_condition

--- test_day08.py
+++ test_day08.py
@@ -7,4 +7,13 @@ log = get_logger(__name__)
 
 
 class TestDay(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.ra = Register(id='a', value=5)
+        self.rb = Register(id='b', value=3)
+        self.registers = RegisterCollection(registers=[self.ra, self.rb])
+
+    def test_test_conditon(self):
+        cond = 'a == 5'
+        self.assertTrue(self.registers.test_condition(cond))
+        cond = 'a == 4'
+        self.assertFalse(self.registers.test_condition(cond))
