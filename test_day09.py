65c34aca6c92573701bb503ab10f0d4a6b2cd6e5
Lughead <jedmitten@alumni.cmu.edu>
2017-12-17 14:31:57 -0800

    test garbage removal

--- test_day09.py
+++ test_day09.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day08 import *
+from day09 import *
 from common import get_logger
 
 log = get_logger(__name__)
@@ -11,23 +11,31 @@ SCORE = 'score'
 
 class TestDay(unittest.TestCase):
     def setUp(self):
-        inputs = {
+        self.inputs = {
             1: {TEXT: '{}',
-                SCORE: 1}, 
+                SCORE: 1},
             2: {TEXT: '{{{}}}',
-                SCORE: 6}, 
+                SCORE: 6},
             3: {TEXT: '{{},{}}',
                 SCORE: 6},
             4: {TEXT: '{{{},{},{{}}}}',
                 SCORE: 16},
-            5: {TEXT: '{<a>,<a>,<a>,<a>}', 
+            5: {TEXT: '{<a>,<a>,<a>,<a>}',
                 SCORE: 1},
             6: {TEXT: '{{<ab>},{<ab>},{<ab>},{<ab>}}',
                 SCORE: 9},
             7: {TEXT: '{{<!!>},{<!!>},{<!!>},{<!!>}}',
                 SCORE: 9},
-            8: {TEXT: '{{<a!>},{<a!>},{<a!>},{<ab>}}', 
+            8: {TEXT: '{{<a!>},{<a!>},{<a!>},{<ab>}}',
                 SCORE: 3}
         }
 
-    
+    def test_garbage_cleanup(self):
+        txt = self.inputs.get(5).get(TEXT)
+        self.assertEqual('{,,,}', GarbageCleaner.cleanup_garbage(txt))
+        txt = self.inputs.get(6).get(TEXT)
+        self.assertEqual('{{},{},{},{}}', GarbageCleaner.cleanup_garbage(txt))
+        txt = self.inputs.get(7).get(TEXT)
+        self.assertEqual('{{},{},{},{}}', GarbageCleaner.cleanup_garbage(txt))
+        txt = self.inputs.get(8).get(TEXT)
+        self.assertEqual('{{}}', GarbageCleaner.cleanup_garbage(txt))
