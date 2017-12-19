e1a76b211c93fc9dc6a5bf16378418d15d2f471f
Lughead <jedmitten@alumni.cmu.edu>
2017-12-18 22:28:11 -0800

    impl reverse sub list

--- day10.py
+++ day10.py
@@ -3,6 +3,22 @@ import common
 log = common.get_logger(__name__)
 
 
+def _init_elements():
+    return list(range(256))
+
+
+def reverse_sublist(in_l, start_index, num):
+    sub_l = []
+    for i in range(num):
+        idx = (start_index + i) % len(in_l)
+        sub_l.append(in_l[idx])
+    sub_l.reverse()
+    for i in range(num):
+        idx = (start_index + i) % len(in_l)
+        in_l[idx] = sub_l[i]
+    return in_l
+
+
 def main():
     answer_1 = 'Unknown'
     print('The answer to part 1 is: {}'.format(answer_1))
