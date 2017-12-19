89a3281bbefd482691858143adf119e745c1274c
Lughead <jedmitten@alumni.cmu.edu>
2017-12-19 07:54:44 -0800

    reverse test

--- test_day10.py
+++ test_day10.py
@@ -6,6 +6,18 @@ from common import get_logger
 log = get_logger(__name__)
 
 
+def get_test_inputs():
+    return [3, 4, 1, 5]
+
+
+def init_test_elements():
+    return list(range(5))
+
+
 class TestDay(unittest.TestCase):
     def setUp(self):
-        pass
+        self.elements = init_test_elements()
+        self.inputs = get_test_inputs()
+
+    def test_reverse_sublist(self):
+        reverse_sublist(init_elements(), 0, 2)
