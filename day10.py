b27d4479153811c8839306e82627c6ba6a26eba4
Lughead <jedmitten@alumni.cmu.edu>
2017-12-18 22:13:08 -0800

    stub day 10

---
+++ day10.py
@@ -0,0 +1,98 @@
+import common
+
+log = common.get_logger(__name__)
+
+
+class Group:
+    def __init__(self):
+        self.parent = None
+
+    @property
+    def score(self):
+        p_score = 0
+        if self.parent is not None:
+            p_score = self.parent.score
+        return p_score + 1
+
+
+class StrParser:
+    GARBAGE_START = '<'
+    GARBAGE_END = '>'
+    GROUP_START = '{'
+    GROUP_END = '}'
+    NEGATOR = '!'
+
+    def __init__(self):
+        self.groups = []
+
+    @classmethod
+    def cleanup_garbage(cls, intext):
+        # type: (str) -> (str, int)
+        txt_ptr = 0
+        clean_str = ''
+        in_garbage = False
+        collected_count = 0
+        while txt_ptr < len(intext):  # starting reading all input
+            if in_garbage:
+                if intext[txt_ptr] == cls.NEGATOR:
+                    log.debug('Skipping {} and {} (index: {})'.format(intext[txt_ptr],
+                                                                      intext[txt_ptr + 1],
+                                                                      txt_ptr))
+                    txt_ptr += 1
+                elif intext[txt_ptr] == cls.GARBAGE_END:
+                    in_garbage = False
+                else:
+                    collected_count += 1
+            else:
+                if intext[txt_ptr] == cls.GARBAGE_START:
+                    log.debug('Found start of garbage at character index: {}'.format(txt_ptr))
+                    in_garbage = True
+                else:
+                    clean_str += intext[txt_ptr]
+            txt_ptr += 1
+        return clean_str, collected_count
+
+    @classmethod
+    def build_groups(cls, intext):
+        # type: (str) -> list[int]
+        txt_ptr = 0
+        grp_ptr = -1
+        groups = []
+        while txt_ptr < len(intext):
+            if intext[txt_ptr] == cls.GROUP_START:
+                grp_ptr += 1
+                if len(groups) <= grp_ptr:
+                    groups.append(1)
+                else:
+                    groups[grp_ptr] += 1
+            elif intext[txt_ptr] == cls.GROUP_END:
+                grp_ptr -= 1
+            else:
+                # just skip because we're inside a group
+                pass
+            txt_ptr += 1
+        return groups
+
+    @classmethod
+    def count_group_score(cls, groups):
+        # type: (list[int]) -> int
+        score = 0
+        for i in range(len(groups)):
+            multiplier = i + 1
+            score += multiplier * groups[i]
+        return score
+
+
+def main():
+    text = common.read_input(9)
+    text, g_count = StrParser.cleanup_garbage(text)
+    groups = StrParser.build_groups(text)
+    answer_1 = StrParser.count_group_score(groups)
+    print('The answer to part 1 is: {}'.format(answer_1))
+    answer_2 = g_count
+    print('The answer to part 2 is: {}'.format(answer_2))
+
+
+if __name__ == '__main__':
+    log = common.get_logger('day09', level='INFO')
+    main()
---
+++ test_day10.py
@@ -0,0 +1,11 @@
+import unittest
+
+from day10 import *
+from common import get_logger
+
+log = get_logger(__name__)
+
+
+class TestDay(unittest.TestCase):
+    def setUp(self):
+        pass
