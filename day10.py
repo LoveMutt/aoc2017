e84a5b360de19b028798033edf26b0b60d132948
mittens <jed.mitten@fireeye.com>
2017-12-19 08:45:25 -0800

    impl hashing function

--- day10.py
+++ day10.py
@@ -8,7 +8,7 @@ def init_elements():
     return list(range(256))
 
 
-def reverse_sublist(in_l, start_index, num):
+def reverse_slice(in_l, start_index, num):
     # type: (list[int], int, int) -> list[int]
     sub_l = []
     for i in range(num):
@@ -26,6 +26,27 @@ def get_new_pos(in_l, lengths_l, idx, skip):
     length = lengths_l[idx]
     target_idx = (length + skip) % len(in_l)
     return target_idx
+
+
+def hash(len_l, num_l):
+    # type: (list[int]) -> int
+    # init
+    skip = 0
+    pos = 0
+
+    for i in range(len(len_l)):
+        l = len_l[i]
+        # ignore if longer than input list
+        if l > len(len_l):
+            log.debug('Ignoring index {} ({})'.format(i, len_l[i]))
+            continue
+        # step 1: reverse the order of a slice
+        num_l = reverse_slice(num_l, start_index=pos, num=l)
+        # step 2: advance
+        pos = get_new_pos(in_l=num_l, lengths_l=len_l, idx=pos, skip=skip)
+        # step 3: increase step size by 1
+        skip += 1
+    return len_l[0] * len_l[1]
 
 
 def main():
