18c471833061bfb4970d2f1ba1839eebe37216ca
mittens <jed.mitten@fireeye.com>
2017-12-19 20:29:05 -0800

    add test for dense hash

--- test_day10.py
+++ test_day10.py
@@ -6,21 +6,21 @@ from common import get_logger
 log = get_logger(__name__)
 
 
-def get_test_inputs():
+def get_test_lengths():
     return [3, 4, 1, 5]
 
 
-def init_test_elements():
+def get_test_inputs():
     return [0, 1, 2, 3, 4]
 
 
 class TestDay(unittest.TestCase):
     def setUp(self):
-        self.elements = init_test_elements()
         self.inputs = get_test_inputs()
+        self.lengths = get_test_lengths()
 
     def test_reverse_sublist(self):
-        l_rev = reverse_slice(init_test_elements(), 0, 2)
+        l_rev = reverse_slice(get_test_inputs(), 0, 2)
         self.assertListEqual([1, 0, 2, 3, 4], l_rev)
         l_rev = reverse_slice(l_rev, 2, 2)
         self.assertListEqual([1, 0, 3, 2, 4], l_rev)
@@ -28,4 +28,17 @@ class TestDay(unittest.TestCase):
         self.assertListEqual([4, 0, 3, 2, 1], l_rev)
 
     def test_hash(self):
-        self.assertEqual(12, hash(get_test_inputs(), init_test_elements()))
+        hash_val, _, _ = hash(self.lengths, self.inputs)
+        self.assertEqual(12, hash_val)
+
+    def test_convert_list_to_code(self):
+        self.assertListEqual([49,44,50,44,51], convert_list_to_ascii('1,2,3'))
+
+    def test_hash_n(self):
+        self.assertEqual(hash(l_lengths=self.lengths, l_numbers=self.inputs)[0],
+                         hash_n(l_lengths=self.lengths, l_numbers=self.inputs, n=1)[0])
+        
+    def test_dense_hash(self):
+        input = [65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]
+        vals = dense_hash(input)
+        self.assertEqual(64, vals[0])
