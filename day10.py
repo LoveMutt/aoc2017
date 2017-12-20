884e15b236528f76b5cec5012033d56a09dfe153
mittens <jed.mitten@fireeye.com>
2017-12-19 20:29:36 -0800

    add part2 functions: convert to ascii, dense hash, hash n rounds

--- day10.py
+++ day10.py
@@ -24,12 +24,9 @@ def reverse_slice(in_l, start_index, num):
     return l_copy
 
 
-def hash(l_lengths, l_numbers):
-    # type: (list[int]) -> int
+def hash(l_lengths, l_numbers, skip=0, pos=0):
+    # type: (list[int]) -> (int, int, int)
     # init
-    skip = 0
-    pos = 0
-
     for l in l_lengths:
         # ignore if longer than input list
         if l > len(l_numbers):
@@ -43,7 +40,48 @@ def hash(l_lengths, l_numbers):
         # step 3: increase step size by 1
         skip += 1
     log.info('first two values are {}, {}'.format(l_numbers[0], l_numbers[1]))
-    return l_numbers[0] * l_numbers[1]  # what is the result of multiplying the first two numbers in the list
+    return l_numbers[0] * l_numbers[1], skip, pos  # what is the result of multiplying the first two numbers in the list
+
+
+def hash_n(l_lengths, l_numbers, skip=0, pos=0, n=64):
+    # type: (list[int], list[int], int, int) -> (int, int, int)
+    hash_val = -1
+    for i in range(n):
+        hash_val, skip, pos = hash(l_lengths=l_lengths.copy(), l_numbers=l_numbers.copy(), skip=skip, pos=pos)
+    return hash_val, skip, pos
+
+
+def dense_hash(l_numbers):
+    # type: (list[int]) -> list[int]
+    MAGIC = 16
+    l_dense_hash = []
+    for i in range(int(len(l_numbers) / MAGIC)):
+        start = i * MAGIC
+        val = 0
+        for j in range(MAGIC):
+            idx = start + j
+            val ^= l_numbers[idx]
+        l_dense_hash.append(val)
+    return l_dense_hash
+
+
+def convert_to_ord(c):
+    if not isinstance(c, str):
+        c = str(c)
+    return ord(c)
+
+
+def convert_list_to_ascii(s):
+    # type: (str) -> list[int]
+    l_code = []
+    for c in s:
+        l_code.append(convert_to_ord(c))
+    return l_code
+
+
+def get_ascii_sequence(intext):
+    l_inputs = convert_list_to_ascii(intext)
+    l_inputs.extend([17, 31, 73, 47, 23])
 
 
 def main():
@@ -52,7 +90,7 @@ def main():
     l_lengths = [int(n.strip()) for n in intext.split(',')]
     l_numbers = init_elements()
     log.info('Starting hash for part 1...')
-    answer_1 = hash(l_lengths, l_numbers)
+    answer_1, _, _ = hash(l_lengths, l_numbers)
     print('The answer to part 1 is: {}'.format(answer_1))
     answer_2 = 'Unknown'
     print('The answer to part 2 is: {}'.format(answer_2))
