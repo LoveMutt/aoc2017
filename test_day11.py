3878a28abe99b4736c90adfa264e94b479213b53
Lughead <jedmitten@alumni.cmu.edu>
2017-12-21 23:27:27 -0800

    basic tests for HexGrid move and distance

--- test_day11.py
+++ test_day11.py
@@ -14,6 +14,62 @@ def get_test_inputs():
     return [0, 1, 2, 3, 4]
 
 
+STR = 'string'
+DIS = 'distance'
+
+
 class TestDay(unittest.TestCase):
     def setUp(self):
-        pass
+        self.test_inputs = [
+            {STR: 'ne,ne,ne', DIS: 3},
+            {STR: 'ne,ne,sw,sw', DIS: 0},
+            {STR: 'ne,ne,s,s', DIS: 2},
+            {STR: 'se,sw,se,sw,sw', DIS: 3},
+        ]
+
+    def test_parse_input(self):
+        for d in self.test_inputs:
+            self.assertIsInstance(parse_input(d.get(STR)), list)
+
+    def test_move(self):
+        hg = HexGrid()
+        hg.move('n')
+        self.assertTupleEqual(hg.coordinates, (0, 1))
+
+        hg.reset()
+        hg.move('s')
+        self.assertTupleEqual(hg.coordinates, (0, -1))
+
+        hg.reset()
+        hg.move('e')
+        self.assertTupleEqual(hg.coordinates, (1, 0))
+
+        hg.reset()
+        hg.move('w')
+        self.assertTupleEqual(hg.coordinates, (-1, 0))
+
+        hg.reset()
+        hg.move('se')
+        self.assertTupleEqual(hg.coordinates, (1, -1))
+
+        hg.reset()
+        hg.move('sw')
+        self.assertTupleEqual(hg.coordinates, (-1, -1))
+
+        hg.reset()
+        hg.move('ne')
+        self.assertTupleEqual(hg.coordinates, (1, 1))
+
+        hg.reset()
+        hg.move('nw')
+        self.assertTupleEqual(hg.coordinates, (-1, 1))
+
+    def test_distance(self):
+        hg = HexGrid()
+        hg.move('n')
+        self.assertEqual(1, hg.distance)
+        hg.move('n')
+        self.assertEqual(2, hg.distance)
+        hg.move('s')
+        hg.move('s')
+        self.assertEqual(0, hg.distance)
