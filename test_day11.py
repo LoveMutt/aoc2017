c3f79b007cd3bbef525495b904dcdd6b743d4522
Lughead <jedmitten@alumni.cmu.edu>
2017-12-22 00:43:08 -0800

    fix tests so the top of the hexagon is flat instead of pointed

--- test_day11.py
+++ test_day11.py
@@ -5,15 +5,6 @@ from common import get_logger
 
 log = get_logger(__name__)
 
-
-def get_test_lengths():
-    return [3, 4, 1, 5]
-
-
-def get_test_inputs():
-    return [0, 1, 2, 3, 4]
-
-
 STR = 'string'
 DIS = 'distance'
 
@@ -32,44 +23,44 @@ class TestDay(unittest.TestCase):
             self.assertIsInstance(parse_input(d.get(STR)), list)
 
     def test_move(self):
+        # See http://keekerdc.com/wp-content/uploads/2011/03/HexGridLandscapeTriCoordinates.gif
+        # Rotate the axes in that image by 30 degrees to the right (so the flat top is North)
         hg = HexGrid()
         hg.move('n')
-        self.assertTupleEqual(hg.coordinates, (0, 1))
+        self.assertTupleEqual(hg.coordinates, (0, 1, -1))
 
         hg.reset()
         hg.move('s')
-        self.assertTupleEqual(hg.coordinates, (0, -1))
+        self.assertTupleEqual(hg.coordinates, (0, -1, 1))
 
         hg.reset()
         hg.move('e')
-        self.assertTupleEqual(hg.coordinates, (1, 0))
+        self.assertTupleEqual(hg.coordinates, (1, -1, 0))
 
         hg.reset()
         hg.move('w')
-        self.assertTupleEqual(hg.coordinates, (-1, 0))
+        self.assertTupleEqual(hg.coordinates, (-1, 1, 0))
 
         hg.reset()
         hg.move('se')
-        self.assertTupleEqual(hg.coordinates, (1, -1))
+        self.assertTupleEqual(hg.coordinates, (1, -2, 1))
 
         hg.reset()
         hg.move('sw')
-        self.assertTupleEqual(hg.coordinates, (-1, -1))
+        self.assertTupleEqual(hg.coordinates, (-1, 0, 1))
 
         hg.reset()
         hg.move('ne')
-        self.assertTupleEqual(hg.coordinates, (1, 1))
+        self.assertTupleEqual(hg.coordinates, (1, 0, -1))
 
         hg.reset()
         hg.move('nw')
-        self.assertTupleEqual(hg.coordinates, (-1, 1))
+        self.assertTupleEqual(hg.coordinates, (-1, 2, -1))
 
     def test_distance(self):
-        hg = HexGrid()
-        hg.move('n')
-        self.assertEqual(1, hg.distance)
-        hg.move('n')
-        self.assertEqual(2, hg.distance)
-        hg.move('s')
-        hg.move('s')
-        self.assertEqual(0, hg.distance)
+        for d_inputs in self.test_inputs:
+            hg = HexGrid()
+            l_dirs = parse_input(d_inputs.get(STR))
+            distance = d_inputs.get(DIS)
+            hg.follow(l_dirs)
+            self.assertEqual(distance, hg.distance)
