76b26b624bfc084c87ae121b02ee710bfb2fcf34
Lughead <jedmitten@alumni.cmu.edu>
2017-12-26 23:25:47 -0800

    testing for helper methods

--- test_day12.py
+++ test_day12.py
@@ -6,6 +6,33 @@ from common import get_logger
 log = get_logger(__name__)
 
 
+T_STR = '''0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5'''
+
+
 class TestDay(unittest.TestCase):
-    def setUp(self):
-        pass
+    def test_program_generator(self):
+        pid_relations = parse_input(T_STR)
+        programs = program_generator(pid_relations)
+        self.assertIsInstance(programs[0], set)
+        self.assertSetEqual({2}, programs[0])
+        self.assertIsInstance(programs[2], set)
+        self.assertIn(0, programs[2])
+
+    def test_knows(self):
+        progs = program_generator(parse_input(T_STR))
+        self.assertTrue(knows(progs, 2, 0))
+        self.assertTrue(knows(progs, 0, 2))
+        self.assertFalse(knows(progs, 1, 2))
+        self.assertTrue(knows(progs, 0, 4))
+
+        num_links_zero = 0
+        for p in progs:
+            if knows(progs, p, 0):
+                num_links_zero += 1
+        self.assertEqual(6, num_links_zero)
